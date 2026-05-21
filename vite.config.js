42ef3b76bd294acfd490ebcda92dc3144a283f2e
JoseeMelk <moralescastellanos1234@gmail.com>
2026-05-20 18:11:48 -0600

    fix(vite): add reference on vite config

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,37 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                // CSS base
+                'resources/css/app.css',
+                'resources/css/nav/nav-logo.css',
+                'resources/css/nav/nav-auth.css',
+                'resources/css/layout/sidebar.css',
+                'resources/css/common/typography.css',
+                'resources/css/public/catalog.css',
+
+                // JS base
+                'resources/js/app.js',
+
+                // Auth
+                'resources/js/auth/register.js',
+                'resources/js/auth/login.js',
+                'resources/js/auth/logout.js',
+
+                // Layout
+                'resources/js/layout/sidebar.js',
+
+                // Public
+                'resources/js/public/catalog/catalog-page.js',
+
+                // Provider
+                'resources/js/provider/profile/update.js',
+                'resources/js/provider/product/page/index.js',
+
+                // Admin
+                'resources/js/admin/user/index.js',
+                'resources/js/admin/user/approve-reject.js',
+            ],
             refresh: true,
         }),
         tailwindcss(),
