afc22d91c9c3e6738c5bdc99e45e45195f8ef340
JoseeMelk <moralescastellanos1234@gmail.com>
2026-05-24 17:37:15 -0600

    fix: add db transaction in the register

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -5,7 +5,9 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\StoreRegisterRequest;
 use App\Models\User;
+use App\Services\MailService;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\DB;
 
 class RegisterController extends Controller
 {
@@ -14,18 +16,23 @@ class RegisterController extends Controller
         return view('auth.register');
     }
     
-    public function store(StoreRegisterRequest $request)
+    public function store(StoreRegisterRequest $request, MailService $mailService)
     {
         $response = ['ok' => false, 'message' => 'Error al registrar.'];
         $statusCode = 422;
         try {
             $data = $request->validated();
             $data['status'] = 'pending'; // Establecer estado como pendiente
+            DB::beginTransaction();
             $user = User::create($data);
             $user -> assignRole('provider');
+            // Enviar correo de bienvenida pendiente
+            //$mailService->sendWelcomePending($user);
+            DB::commit();
             $response = ['ok' => true, 'message' => 'Registro exitoso. Tu cuenta está pendiente de aprobación.'];
             $statusCode = 201;
         } catch (\Exception $e) {
+            DB::rollBack();
             Log::error('Error al registrar usuario: ' . $e->getMessage());
         }
         return response()->json($response, $statusCode);
